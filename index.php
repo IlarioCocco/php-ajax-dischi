5cbd92bdb7b5d7f5249184e08b66c700499a8ded
IlarioCOCCO <ilario.cocco@gmail.com>
2021-10-21 17:13:19 +0200

    degub esercizio

--- index.php
+++ index.php
@@ -72,14 +72,15 @@ $database = [
     <main>
         <container class="box">
             <?php foreach ($database as $valore) { ?>
-                
                 <p><?php echo "{$valore["title"]} {$valore["author"]} {$valore["year"]} {$valore["poster"]}"; ?></p>
 
-                <div class="">
-                    <img  src="<?php echo "{$valore["poster"]}"?>" alt="">
-                    <h3><?php echo "{$valore["title"]}"?></h3>
+                <div class="imaginiAlbum">
+                    <img src="<?php echo "{$valore["poster"]}" ?>" alt="">
+                    <h3><?php echo "{$valore["title"]}" ?></h3>
+                    <p><?php echo "{$valore["author"]}" ?></p>
+                    <p><?php echo "{$valore["year"]}" ?></p>
                 </div>
-               
+
             <?php } ?>
         </container>
     </main>
