74406593425546a08e30b4f5c97f09ea77c1bed3
IlarioCOCCO <ilario.cocco@gmail.com>
2021-10-23 17:31:55 +0200

    add die; for resolve

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 // phpinfo();
 require __DIR__ . "/database.php";
 header("Content-Type: application/json");
-echo json_encode($database); 
+echo json_encode($database); die;
 // var_dump ($database);
 // var_dump (json_encode ($database)); die;
 // il var_dump si utilizza per vedere il tipo di dato(array)
