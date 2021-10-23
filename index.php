57d66bb8899c0bf7cf5ceea50f425efb8c698b5a
IlarioCOCCO <ilario.cocco@gmail.com>
2021-10-23 17:27:47 +0200

    found in debug :SyntaxError: JSON.parse: unexpected non-whitespace character after JSON data

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 // phpinfo();
 require __DIR__ . "/database.php";
 header("Content-Type: application/json");
-echo json_encode($database); die;
+echo json_encode($database); 
 // var_dump ($database);
 // var_dump (json_encode ($database)); die;
 // il var_dump si utilizza per vedere il tipo di dato(array)
