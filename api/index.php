556ab8bd006aa651ba1046c82984da851b5ee72c
IlarioCOCCO <ilario.cocco@gmail.com>
2021-10-24 00:30:05 +0200

    add function of genre filtered

--- api/index.php
+++ api/index.php
@@ -1,6 +1,22 @@
 <?php
 require __DIR__ . "/../database.php";
 header("Content-Type: application/json");
-echo json_encode($database); 
+
+if( !empty($_GET["genre"])) {
+    $genre = $_GET["genre"];
+
+    $databaseFiltered = [];
+
+    foreach($database as $database) {
+        if($genre == $database["genre"]) {
+
+            $databaseFiltered[] = $database;
+        }
+    }
+    echo json_encode($databaseFiltered); 
+} else {
+    echo json_encode($database); 
+}
+
 ?>
 
